ba782ba7bb24e96ebdbcfecde33526f7d809c860
Sharunikaa13 <sharunikaa.ss@surveysparrow.com>
2025-12-13 23:55:18 +0530

    updated

--- personal/leetcode/Array/longest_common_prefix/pointer.py
+++ personal/leetcode/Array/longest_common_prefix/pointer.py
@@ -2,7 +2,7 @@ class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         r=""
         s=sorted(strs)
-        a=[len(strs[i]) for i in range(len(strs))]
+        a=min(strs,key=len)
         b=min(a)
         first, last = s[0], s[-1]
         for j in range(b):
